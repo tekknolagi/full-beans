0beac7ac035dc554240c559320b3604300685151
Max Bernstein <max@bernsteinbear.com>
2024-09-06 13:19:15 -0400

    Handle fenster input, poorly

--- demo/main.c
+++ demo/main.c
@@ -203,25 +203,6 @@ static void process_frame(mu_Context *ctx) {
 }
 
 
-
-static const char button_map[256] = {
-  [ SDL_BUTTON_LEFT   & 0xff ] =  MU_MOUSE_LEFT,
-  [ SDL_BUTTON_RIGHT  & 0xff ] =  MU_MOUSE_RIGHT,
-  [ SDL_BUTTON_MIDDLE & 0xff ] =  MU_MOUSE_MIDDLE,
-};
-
-static const char key_map[256] = {
-  [ SDLK_LSHIFT       & 0xff ] = MU_KEY_SHIFT,
-  [ SDLK_RSHIFT       & 0xff ] = MU_KEY_SHIFT,
-  [ SDLK_LCTRL        & 0xff ] = MU_KEY_CTRL,
-  [ SDLK_RCTRL        & 0xff ] = MU_KEY_CTRL,
-  [ SDLK_LALT         & 0xff ] = MU_KEY_ALT,
-  [ SDLK_RALT         & 0xff ] = MU_KEY_ALT,
-  [ SDLK_RETURN       & 0xff ] = MU_KEY_RETURN,
-  [ SDLK_BACKSPACE    & 0xff ] = MU_KEY_BACKSPACE,
-};
-
-
 static int text_width(mu_Font font, const char *text, int len) {
   if (len == -1) { len = strlen(text); }
   return r_get_text_width(text, len);
@@ -233,7 +214,6 @@ static int text_height(mu_Font font) {
 
 
 int main(int argc, char **argv) {
-  /* init SDL and renderer */
   r_init();
   struct fenster *window = (struct fenster *)r_window();
 
@@ -243,33 +223,23 @@ int main(int argc, char **argv) {
   ctx->text_width = text_width;
   ctx->text_height = text_height;
 
+  int fps = 60;
+
   /* main loop */
   for (;;) {
-    /* handle SDL events */
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-      switch (e.type) {
-        case SDL_QUIT: exit(EXIT_SUCCESS); break;
-        case SDL_MOUSEMOTION: mu_input_mousemove(ctx, e.motion.x, e.motion.y); break;
-        case SDL_MOUSEWHEEL: mu_input_scroll(ctx, 0, e.wheel.y * -30); break;
-        case SDL_TEXTINPUT: mu_input_text(ctx, e.text.text); break;
-
-        case SDL_MOUSEBUTTONDOWN:
-        case SDL_MOUSEBUTTONUP: {
-          int b = button_map[e.button.button & 0xff];
-          if (b && e.type == SDL_MOUSEBUTTONDOWN) { mu_input_mousedown(ctx, e.button.x, e.button.y, b); }
-          if (b && e.type ==   SDL_MOUSEBUTTONUP) { mu_input_mouseup(ctx, e.button.x, e.button.y, b);   }
-          break;
-        }
-
-        case SDL_KEYDOWN:
-        case SDL_KEYUP: {
-          int c = key_map[e.key.keysym.sym & 0xff];
-          if (c && e.type == SDL_KEYDOWN) { mu_input_keydown(ctx, c); }
-          if (c && e.type ==   SDL_KEYUP) { mu_input_keyup(ctx, c);   }
-          break;
-        }
-      }
+    if (f->mouse) {
+      mu_input_mousedown(ctx, f->x, f->y, MU_MOUSE_LEFT);
+    } else {
+      // TODO(max): catch mouse up from state change
+    }
+    // TODO(max): maybe only do this on state change
+    mu_input_mousemove(ctx, f->x, f->y);
+    // TODO(max): scroll
+    if (f->keys[0x1b]) { break; }  // esc
+    for (int i = 0; i < 256; i++) {
+      if (f->keys[i]) { mu_input_keydown(ctx, i); }
+      else { } // TODO(max): catch key up from state change
+      // TODO(max): mod
     }
 
     /* process frame */
