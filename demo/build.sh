28058e7c4ff0e1a88d5489ae4f63dcc0d0177d2c
Max Bernstein <max@bernsteinbear.com>
2024-09-06 13:05:41 -0400

    Initial commit

---
+++ demo/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OS_NAME=`uname -o 2>/dev/null || uname -s`
+
+if [ $OS_NAME == "Msys" ]; then
+    GLFLAG="-lgdi32"
+elif [ $OS_NAME == "Darwin" ]; then
+    GLFLAG="-framework Cocoa"
+else
+    GLFLAG="-lX11"
+fi
+
+CFLAGS="-I../ -Wall -std=c11 -pedantic $GLFLAG -lm -O2 -g"
+
+gcc main.c renderer.c ../src/microui.c $CFLAGS
+
